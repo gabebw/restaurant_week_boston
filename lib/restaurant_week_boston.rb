7f14f7870fa253218fad1cacd03b3acdd59fd1a1
Gabe Berke-Williams <gbw@brandeis.edu>
2010-09-12 21:24:27 -0400

    Add VERSION constant

--- lib/restaurant_week_boston.rb
+++ lib/restaurant_week_boston.rb
@@ -1,4 +1,6 @@
 require 'rubygems'
+
+require 'restaurant_week_boston/version'
 require 'restaurant_week_boston/scraper'
 require 'restaurant_week_boston/marker'
 require 'restaurant_week_boston/restaurant'
---
+++ lib/restaurant_week_boston/version.rb
@@ -0,0 +1,3 @@
+module RestaurantWeekBoston
+  VERSION="1.1.0"
+end
